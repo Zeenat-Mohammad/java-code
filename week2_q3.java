8dd591f548a40d90f0fc14a8a7996ef01d987faa
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2026-06-11 17:17:24 +0300

    Create week2_q3.java

---
+++ week2_q3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class week2_q3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double sum=0.0;
+        for (int i=1;i<=n;i++) {
+            sum+=1.0/i;
+        }
+        System.out.println("Sum of reciprocals: "+sum);
+    }
+}

47dfe0d75779b96d6481f3dee95bae5777f83a5f
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2026-06-11 17:14:43 +0300

    Create week2_q2.java

---
+++ week2_q2.java
@@ -0,0 +1,8 @@
+public class week2_q2 {
+    public static void main(String[] args) {
+        System.out.println("Number\tSquare\tCube");
+        for (int i=1;i<=5;i++) {
+            System.out.println(i+"\t"+(i*i)+"\t"+(i*i*i));
+        }
+    }
+}

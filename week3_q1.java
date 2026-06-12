b4a7106ba05cdd5cf416636ab725ea21b41702e7
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2026-06-12 18:00:14 +0300

    week3_q1.java

---
+++ week3_q1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner
+class week3_q1 {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n = 10, 
+    for (int i = 1; i <= n; i++) {
+      System.out.print(firstTerm + ", ");
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+  }
+}

ec1dc45c0a1fa03e7130ea2ea3b616d99550c627
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2026-06-16 18:44:30 +0300

    Add files via upload

--- week3_q1.java
+++ week3_q1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner
 class week3_q1 {
   public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
+    Scanner sc=new Scanner(System.in); 
     int n = 10, 
     for (int i = 1; i <= n; i++) {
       System.out.print(firstTerm + ", ");

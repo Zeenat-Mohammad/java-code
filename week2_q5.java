9740b03df7be243af5fe903dceddbc79269821cf
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2026-06-17 18:47:16 +0300

    Add files via upload

--- week2_q5.java
+++ week2_q5.java
@@ -5,7 +5,7 @@ public class FloydAndPattern {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int num=1;
-        System.out.println("Floyd's triangle");
+        System.out.println("Floyd's triangle"); 
         for (int i=1;i<=n;i++) {
             for (int j =1;j<=i;j++) {
                 System.out.print(num+" ");

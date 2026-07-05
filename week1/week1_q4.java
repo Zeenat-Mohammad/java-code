d4680a604cf524f73dcd354177fd1fa28eb5b713
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2026-07-05 18:01:54 +0300

    Add files via upload

--- week1/week1_q4.java
+++ week1/week1_q4.java
@@ -1,15 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.app;
-
 import java.util.Scanner;
-
-/**
- *
- * @author zeena
- */
 public class week1_q4 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);

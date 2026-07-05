16e83be07336cf0a66037bce63af423d7b032e45
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2026-07-05 17:57:50 +0300

    Add files via upload

---
+++ week1/week1_q1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class week1_q1 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Address: ");
+        String address=sc.nextLine();
+        System.out.println(address);
+    }
+}

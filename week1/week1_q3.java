5d782752bb0867b0d5dc3c7889f3860d2959afe1
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2026-07-05 18:00:52 +0300

    Add files via upload

--- week1/week1_q3.java
+++ week1/week1_q3.java
@@ -3,7 +3,7 @@ public class week1_q3 {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number: ");
-        int n=sc.nextInt();
+        int n=sc.nextInt();  
         int arr[]=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();

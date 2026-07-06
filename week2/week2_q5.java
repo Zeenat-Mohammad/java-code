424daca5c6be877125ad3ba2800b30b66b4fe5c6
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2026-07-06 18:58:46 +0300

    Add files via upload

--- week2/week2_q5.java
+++ week2/week2_q5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FloydAndPattern {
+public class week2_q5 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();

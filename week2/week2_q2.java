ad06239a7212fb936c544b95625ebb71a48d9481
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2026-07-06 18:56:36 +0300

    Add files via upload

--- week2/week2_q2.java
+++ week2/week2_q2.java
@@ -1,7 +1,7 @@
 public class week2_q2 {
     public static void main(String[] args) {
         System.out.println("Number\tSquare\tCube");
-        for (int i=1;i<=5;i++) {
+        for (int i=1; i<=5;i++) {
             System.out.println(i+"\t"+(i*i)+"\t"+(i*i*i)); 
         }
     }

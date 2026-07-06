0d8e71ec88f68e7ef059f0413fa86463ed4fe92c
Zeenat-Mohammad <135528133+Zeenat-Mohammad@users.noreply.github.com>
2026-07-06 19:01:02 +0300

    Add files via upload

--- week3/week3_q3.java
+++ week3/week3_q3.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class week3_q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int sum = 0, product = 1, n = 4;
-
         for (int i = 1; i <= n; i++) {
             int num = sc.nextInt();
             sum += num;
             product *= num;
         }
-
         System.out.println("Sum: " + sum);
         System.out.println("Product: " + product);
     }
